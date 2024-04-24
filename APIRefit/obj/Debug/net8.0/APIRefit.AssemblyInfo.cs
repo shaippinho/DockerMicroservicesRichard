0d0832a7cb809a9f65ff54ca5ac7d21de0eaaeb7
Richard Vepo <richardvepogg@gmail.com>
2024-04-23 22:16:28 -0300

    criação dos dockerfile e dockercompose

--- APIRefit/obj/Debug/net8.0/APIRefit.AssemblyInfo.cs
+++ APIRefit/obj/Debug/net8.0/APIRefit.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIRefit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bbd9bff434fb175b8006d75fc8d10633714dcb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIRefit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIRefit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- AcessoDados/obj/Debug/net8.0/AcessoDados.AssemblyInfo.cs
+++ AcessoDados/obj/Debug/net8.0/AcessoDados.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AcessoDados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bbd9bff434fb175b8006d75fc8d10633714dcb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AcessoDados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AcessoDados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
